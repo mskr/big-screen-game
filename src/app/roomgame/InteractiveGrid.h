832fdea3ea367ae0d36d6ffe05ad88c94d3c9698
Viscom Lab <nothing>
2017-06-29 15:30:35 +0200

    More comments

--- src/app/roomgame/InteractiveGrid.h
+++ src/app/roomgame/InteractiveGrid.h
@@ -29,20 +29,30 @@ protected:
 	// Input-related members
 	glm::dvec2 last_mouse_position_;
 	std::list<GridInteraction*> interactions_;
+
 public:
+
+    /* Computes cell positions by iteratively adding (height/rows, height/rows) to (-1, -1) */
 	InteractiveGrid(size_t columns, size_t rows, float height);
 	~InteractiveGrid();
+
+
 	// Helper functions
 	GridCell* InteractiveGrid::getClosestWallCell(glm::vec2 pos);
 	void forEachCell(std::function<void(GridCell*)> callback);
 	void forEachCell(std::function<void(GridCell*,bool*)> callback);
 	void forEachCellInRange(GridCell* leftLower, GridCell* rightUpper, std::function<void(GridCell*)> callback);
 	void forEachCellInRange(GridCell* leftLower, GridCell* rightUpper, std::function<void(GridCell*,bool*)> callback);
+
+    /* Binary search on cells */
 	GridCell* getCellAt(glm::vec2 positionNDC);
+
 	bool isInsideGrid(glm::vec2 positionNDC);
 	bool isInsideCell(glm::vec2 positionNDC, GridCell* cell);
 	bool isColumnEmptyBetween(size_t col, size_t startRow, size_t endRow);
 	bool isRowEmptyBetween(size_t row, size_t startCol, size_t endCol);
+
+
 	// Getters
 	float getCellSize();
 	size_t getNumColumns();
@@ -50,18 +60,28 @@ public:
 	size_t getNumCells();
 	GridCell* getCellAt(size_t col, size_t row);
 	glm::vec3 getTranslation() { return translation_; }
+
+
 	// Render functions
 	void uploadVertexData();
 	virtual void loadShader(viscom::GPUProgramManager mgr);
 	void onFrame();
 	void cleanup();
 	void translate(float dx, float dy, float dz);
+
+
 	// Input and interaction functions
 	virtual void onTouch(int touchID);
 	virtual void onRelease(int touchID);
 	virtual void onMouseMove(int touchID, double newx, double newy);
+
+    /* Transform original cell position into what the user sees */
 	glm::vec2 getNDC(glm::vec2 position);
+
+    /* Remember camera projection for later being able to perform cell selection on user input */
 	void updateProjection(glm::mat4&);
+
+
 	// Functions for grid modification
 	virtual void buildAt(size_t col, size_t row, GridCell::BuildState buildState);
 	void buildAtLastMousePosition(GridCell::BuildState buildState);
