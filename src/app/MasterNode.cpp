86c5c3232f032b4425660d915fac1cdd0d058dc5
lab <no@email.com>
2017-08-10 14:10:22 +0200

    Multitouch is working

--- src/app/MasterNode.cpp
+++ src/app/MasterNode.cpp
@@ -330,17 +330,14 @@ namespace viscom {
                 input tmp = inputBuffer.at(i);
                 viscom::math::Line3<float> ray = GetCamera()->GetPickRay({ tmp.x,tmp.y });
                 if (tmp.type == INPUT_UPDATE) {
-                    //std::cout << "DEBUG: update curser at pos " << tmp.x << " " << tmp.y << std::endl;
-                    grid_.onMouseMove(-1, ray[0], ray[1]);
+                    grid_.onMouseMove(tmp.id, ray[0], ray[1]);
                 }
                 else if (tmp.type == INPUT_ADD) {
-                    //std::cout << "DEBUG: Added curser at pos  "<< tmp.x << " " << tmp.y << std::endl;
-                    grid_.onMouseMove(-1, ray[0], ray[1]);
+                    grid_.onMouseMove(tmp.id, ray[0], ray[1]);
                     grid_.onTouch(tmp.id);
                 }
                 else if (tmp.type == INPUT_REMOVE) {
-                    //std::cout << "DEBUG: removed curser at pos " << tmp.x << " " << tmp.y << std::endl;
-                    grid_.onMouseMove(-1, ray[0], ray[1]);
+                    grid_.onMouseMove(tmp.id, ray[0], ray[1]);
                     grid_.onRelease(tmp.id);
                 }
             }
